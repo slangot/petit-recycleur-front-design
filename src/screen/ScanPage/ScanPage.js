9b3975cd5e15d210696a572f359e7eb1d3544966
Shawn <shawn17@live.fr>
2022-01-09 23:25:16 +0100

    fixing result scan page, adding and fixing style

--- src/screen/ScanPage/ScanPage.js
+++ src/screen/ScanPage/ScanPage.js
@@ -18,6 +18,8 @@ const ScanPage = () => {
 
   // states
   const [camera, setCamera] = useState(false)
+  //const [codeBar, setCodeBar] = useState(false)
+  //const [codeBarResult, setCodeBarResult] = useState(null)
   const [showScanButton, setShowScanButton] = useState(true)
 
   const [result, setResult] = useState(null)
@@ -63,6 +65,18 @@ const ScanPage = () => {
     setShowScanButton(false)
   }
 
+  // const handleSetCodeBar = () => {
+  //   setCodeBar(true)
+  // }
+
+  // const handleCodeBarResult = (code) => {
+  //   setCodeBarResult(code)
+  // }
+
+  const handleSetResult = (code) => {
+    setResult(code)
+  }
+
   // Convert the scanning number from Quagga and updating the result state
   const onDetected = result => {
     setResult(result);
@@ -93,6 +107,7 @@ const ScanPage = () => {
         Quagga.stop()
         setCamera(false)
         setShowScanButton(false)
+        //setCodeBar(false)
       })
     } catch(error) {
       console.log(error)
@@ -110,10 +125,12 @@ const ScanPage = () => {
     Quagga.stop()
     setCamera(false)
     setShowScanButton(false)
+    //setCodeBar(false)
   }
 
   const handleRetry = () => {
     setResult(null)
+    //setCodeBarResult(null)
     setTimeout(() => {
       setCamera(true)
     }, 1000)
@@ -142,13 +159,22 @@ const ScanPage = () => {
                   <h4>Passer votre code barre pour le scanner</h4>
                   <img src={Scan2Img} className="img-fluid" alt="scan logo" />
                 </div>
+              // :
+              // codeBar ?
+              // <div className="col-12 col-lg-3">
+              //     <h4>Entrez le code barre</h4>
+              //     {/* <img src={Scan2Img} className="img-fluid" alt="scan logo" /> */}
+              //   </div>
               :
               <div className="col-12 col-lg-4">
-                <h2 className="display-4 lh-1 mb-4">Scanner votre produit</h2>
+                <h2 className="display-4 lh-1 mb-4">Trouver votre produit</h2>
                 {showScanButton ? 
-                  <button className='btn btn-success btn-lg to-scan-button' onClick={() => setCamera(true)}>Scanner</button> 
+                <div className="scan-choice-button">
+                  <button className='btn btn-outline-success btn-lg to-scan-button' onClick={() => setCamera(true)}>Scanner</button> 
+                  {/* <button className='btn btn-outline-success btn-lg to-scan-button' onClick={() => setCodeBar(true)}>Saisir code</button>  */}
+                </div>
                   :
-                  <button className='btn btn-outline-success to-scan-button' onClick={handleReload}>Scanner un autre produit</button>
+                  <button className='btn btn-outline-success to-scan-button' onClick={() => handleReload()}>Scanner un autre produit</button>
                 }
               </div>
               }
@@ -161,6 +187,12 @@ const ScanPage = () => {
                     </div>
                   </div>
                 </div>
+                // :
+                // codeBar ?
+                // <div className="col-12 col-lg-9 col-sm-8 col-md-6">
+                //   <input type='number' onChange={(e) => handleCodeBarResult(e.target.value)} />
+                //   <button className='to-search-button' onClick={() => handleSetResult(codeBarResult)}>Rechercher</button>
+                // </div>
                 :
                 <div className="col-sm-8 col-md-6">
                   <div className="px-5 px-sm-0"><img className="img-fluid scan-img" src={ScanImg} alt="scan logo" /></div>
@@ -177,162 +209,88 @@ const ScanPage = () => {
                 </div>
               </div>
               <div className="col-sm-8 col-md-6 col-lg-7">
-              <h2 className="display-4 lh-1 mb-4">Produit non trouvé</h2>
-              <h4 className="lh-1 mb-2">Que faire ?</h4>
-              <div className="d-flex justify-content-around mb-4">
-                <button className="btn btn-outline-success" onClick={() => handleRetry()}>
-                  <p>Réessayer</p>
-                  <i class="bi bi-arrow-counterclockwise scan-icons"></i>
-                </button>
-                <button className="btn btn_custom_brown">
-                  <p>Entrez le code</p>
-                  <i class="bi bi-pencil-square scan-icons"></i>
-                </button>
-                <a href="/contribute">
-                  <button className="btn btn_custom_gold">
-                    <p>Contribuer</p>
-                    <i class="bi bi-folder-plus scan-icons"></i>
+                <h2 className="display-4 lh-1 mb-4">Produit non trouvé</h2>
+                <h4 className="lh-1 mb-2">Que faire ?</h4>
+                <div className="d-flex justify-content-around mb-4">
+                  <button className="btn btn-outline-success" onClick={() => handleRetry()}>
+                    <p>Réessayer</p>
+                    <i class="bi bi-arrow-counterclockwise scan-icons"></i>
                   </button>
-                </a>
-              </div>
-              <p><i class="bi bi-exclamation-triangle-fill scan-icons scan-icons-danger"></i> Il se peut que votre produit ne soit pas alimentaire, ou qu'il ne figure pas encore dans nos données.</p>
+                  {/* <button className="btn btn_custom_brown" onClick={handleSetCodeBar()}> */}
+                  <button className="btn btn_custom_brown">
+                    <p>Entrez le code</p>
+                    <i class="bi bi-pencil-square scan-icons"></i>
+                  </button>
+                  <a href="/contribute">
+                    <button className="btn btn_custom_gold">
+                      <p>Contribuer</p>
+                      <i class="bi bi-folder-plus scan-icons"></i>
+                    </button>
+                  </a>
+                </div>
+                <p><i class="bi bi-exclamation-triangle-fill scan-icons scan-icons-danger"></i> Il se peut que votre produit ne soit pas alimentaire, ou qu'il ne figure pas encore dans nos données.</p>
               </div>
             </div>
-
-
 
             : responseStatus &&
-            <div className="row gx-5 align-items-center justify-content-center justify-content-lg-between">
-            <div className="col-12 col-lg-5">
-            <div className='ScanPage-result-container'>
+            <>
+            <div className='ScanPage-result-container row gx-5 align-items-center justify-content-center justify-content-lg-between'>
+            
               <h2>Resultats :</h2>
-              <div className='result-barcode'>
+              <div className="col-lg-5">
                 <h3>Code Barre : </h3>
                 <h4>{result}</h4>
+                {apiRes && 
+                <>
+                {apiRes.data.product.image_packaging_small_url ?
+                  <img src={apiRes.data.product.image_packaging_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
+                  : apiRes.data.product.image_front_small_url ? <img src={apiRes.data.product.image_front_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
+                  : null}
+                </>
+                }
               </div>
               {apiRes &&
-              <div className='result-product-status-container'>
-                <div>
-                  <div className='result-product'>
-                      <div className='result-image'>
-                      {apiRes.data.product.image_packaging_small_url ?
-                        <img src={apiRes.data.product.image_packaging_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
-                        : apiRes.data.product.image_front_small_url ? <img src={apiRes.data.product.image_front_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
-                        : null}
-                      </div>
-                  </div>
-                  <div className='result-status'>
-                    {(apiRes.data.product.packaging_tags || apiRes.data.product.packaging) && recyclingName.some(element => (apiRes.data.product.packaging_tags.includes(element) || apiRes.data.product.packaging.includes(element))) ?
-                    <div>
-                      <div className='result-status-image'>
-                        <img src={RecyclableImg} alt='recyclable' />
-                      </div>
-                      <h3 className='result-status-recyclable'>Emballage <strong>Recyclable</strong></h3>
-                    </div>
-                  : <div>
-                      <div className='result-status-image'>
-                        <img src={NonRecyclableImg} alt='unrecyclable' />
-                      </div>
-                      <h3 className='result-status-unrecyclable'>Emballage <strong>Non Recyclable</strong></h3>
-                    </div>
-                  }
-                  </div>
-                  </div>
-                </div>
-                }
-            </div>
-            </div>
-            </div>
-        }
-
-
-        </div>
-
-
-
-        {/* Show the scan button if we havn't clicked yet */}
-        {showScanButton ? 
-        <button className='to-scan-button' onClick={() => setCamera(true) }>Scanner</button> 
-        :
-        <button className='to-scan-button' onClick={handleReload}>Scanner un autre produit</button>
-        }
-        {/*  */}
-        {camera && 
-          <div className='ScanPage-container'>
-            <div className="container">
-              <Scanner onDetected={onDetected} />
-            </div>
-          </div>
-        }
-
-        {result &&
-          (!responseStatus) ?
-          <div className='ScanPage-result-container not-found-container'>
-            <div className='not-found-img'>
-              <img src={NotFound} alt='not found' />
-            </div>
-            <h3>Produit non trouvé</h3>
-            <p>Vous pouvez réessayer</p>
-            <p>Il se peut que votre produit ne soit pas alimentaire, ou qu'il ne figure pas encore dans nos données.</p>
-          </div>
-          : responseStatus &&
-          <div className='ScanPage-result-container'>
-            <h2>Resultats :</h2>
-            <div className='result-barcode'>
-              <h3>Code Barre : </h3>
-              <h4>{result}</h4>
-            </div>
-            {apiRes &&
-            <div className='result-product-status-container'>
-              <div>
-                <div className='result-product'>
-                    <div className='result-image'>
-                    {apiRes.data.product.image_packaging_small_url ?
-                      <img src={apiRes.data.product.image_packaging_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
-                      : apiRes.data.product.image_front_small_url ? <img src={apiRes.data.product.image_front_small_url || apiRes.data.product.image_front_small_url} alt='produit' />
-                      : null}
-                    </div>
-                </div>
-                <div className='result-status'>
-                  {(apiRes.data.product.packaging_tags || apiRes.data.product.packaging) && recyclingName.some(element => (apiRes.data.product.packaging_tags.includes(element) || apiRes.data.product.packaging.includes(element))) ?
-                  <div>
-                    <div className='result-status-image'>
+              <div className="col-lg-5">
+                {(apiRes.data.product.packaging_tags || apiRes.data.product.packaging) && recyclingName.some(element => (apiRes.data.product.packaging_tags.includes(element) || apiRes.data.product.packaging.includes(element))) ?
+                  <>
+                    <div className='result-status-image mb-4'>
                       <img src={RecyclableImg} alt='recyclable' />
                     </div>
-                    <h3 className='result-status-recyclable'>Emballage <strong>Recyclable</strong></h3>
-                  </div>
-                : <div>
-                    <div className='result-status-image'>
+                    <h3 className='result-status-recyclable'><strong>Recyclable</strong>*</h3>
+                    <p>Pour plus d'informations consulter <a target="_blank" rel="noopener noreferrer" href="https://www.triercestdonner.fr/guide-du-tri">triercestdonner.fr/guide-du-tri</a></p>
+                  </>
+                : <>
+                    <div className='result-status-image mb-4'>
                       <img src={NonRecyclableImg} alt='unrecyclable' />
                     </div>
-                    <h3 className='result-status-unrecyclable'>Emballage <strong>Non Recyclable</strong></h3>
-                  </div>
+                    <h3 className='result-status-unrecyclable'><strong>Non Recyclable</strong></h3>
+                  </>
                 }
-                </div>
-                </div>
               </div>
-              }
+                }
+            </div>
+            <div className="d-flex justify-content-around mt-4 mb-4">
+            <button className="btn btn-outline-success" onClick={() => handleRetry()}>
+              Scanner un autre code
+            </button>
+            {/* <button className="btn btn_custom_brown" onClick={handleSetCodeBar()}> */}
+            <button className="btn btn_custom_brown">
+              Informations sur le recyclage
+            </button>
+            <a href="/contribute">
+              <button className="btn btn_custom_gold">
+                Contribuer au projet
+              </button>
+            </a>
           </div>
+          </>
         }
-    </section>
-    {result &&
-      !apiRes &&
-        // <div className='loader-container'>
-        //   <div className='loader-container-bg'></div>
-        //   <div className='loader-image-container'>
-        //     <img src={LoaderImg} alt="loader" />
-        //   </div>
-        // </div>
-        <div>
-          <div className='bg-opacity'></div>
-          <div className='lds-ring'>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+
+
         </div>
-    }
+
+        </section>
+
       </div>
   );
 }
